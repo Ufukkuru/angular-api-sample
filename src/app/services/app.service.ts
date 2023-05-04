552ee98ff94bf4a1a7b47f206915365899aac32c
ufuk kuru <uege60@gmail.com>
2023-05-04 13:25:10 +0300

    angular callback

--- src/app/services/app.service.ts
+++ src/app/services/app.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
@@ -10,11 +10,25 @@ export class AppService {
     private _http:HttpClient
   ) { }
 
-  get(){
-    return this._http.get("https://jsonplaceholder.typicode.com/todos")
+  get(callback:(res:any) => void){
+    this._http.get("https://jsonplaceholder.typicode.com/todos").subscribe({
+      next:(res)=>{
+        callback(res)
+      },
+      error:(err:HttpErrorResponse)=>{
+        console.log(err)
+      },
+    })
   }
 
-  add(model:any){
-    return this._http.post("https://jsonplaceholder.typicode.com/todos",model,{})
+  add(model:any, callback:(res:any)=> void){
+    this._http.post("https://jsonplaceholder.typicode.com/todos",model,{}).subscribe({
+      next:(res: any) =>{
+        callback(res)
+      },
+      error: (err:HttpErrorResponse)=>{
+        console.log(err)
+      }
+    })
   }
 }
