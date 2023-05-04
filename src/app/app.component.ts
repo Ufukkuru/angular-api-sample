912ede7c624eafc128e46a959941bfbd9a18ac84
ufuk kuru <uege60@gmail.com>
2023-05-04 11:22:37 +0300

    angular with get

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Component } from '@angular/core';
 
 @Component({
@@ -6,5 +7,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'api-sample';
+  constructor(
+    private _http:HttpClient
+  ){
+    this._http.get('https://jsonplaceholder.typicode.com/todos/1').subscribe({
+      next: (res: any) =>{
+        console.log(res)
+      },
+      error: (err:HttpErrorResponse) =>{
+        console.log(err)
+      },
+      complete : () =>{
+
+      }
+    })
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import {CommonModule,} from '@angular/common'
+import {HttpClientModule} from '@angular/common/http'
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    CommonModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
